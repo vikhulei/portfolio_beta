97523aabc0fc933e1249e782440c40c8232b5984
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-01-09 13:11:07 +0200

    Home styles added to separate files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,89 +1,101 @@
-import styled from "styled-components";
-import { Title } from "../styles/Common";
+// import styled from "styled-components";
+// import { Title } from "../styles/Common";
 import me from "../assets/me.png";
 import react from "../assets/react.png";
-import { Button, StylesProvider } from "@material-ui/core";
+import { StylesProvider } from "@material-ui/core";
 import FileIcon from "@material-ui/icons/FileCopy";
 import SendIcon from "@material-ui/icons/Send";
+import {
+  Wrapper,
+  LogoWrapper,
+  TitleHome,
+  Subtitle,
+  ReactImg,
+  PhotoWrapper,
+  Square,
+  Photo,
+  ButtonsWrapper,
+  HomeButton
+} from "../styles/HomeStyles";
 
-const Wrapper = styled.div`
-  position: sticky;
-  top: 0;
-  height: 100vh;
-  width: 100%;
-  background-color: rgb(67, 69, 71);
-  z-index: 10;
-`;
+// const Wrapper = styled.div`
+//   position: sticky;
+//   top: 0;
+//   height: 100vh;
+//   width: 100%;
+//   background-color: rgb(67, 69, 71);
+//   z-index: 10;
+// `;
 
-const LogoWrapper = styled.div`
-  position: absolute;
-  top: 13vh;
-  margin-left: 50%;
-  transform: translate(-50%);
-  text-align: center;
-  height: 140px;
-  width: 100%;
-`;
+// const LogoWrapper = styled.div`
+//   position: absolute;
+//   top: 13vh;
+//   margin-left: 50%;
+//   transform: translate(-50%);
+//   text-align: center;
+//   height: 140px;
+//   width: 100%;
+// `;
 
-const TitleHome = styled(Title)`
-  margin-bottom: 10px;
-  font-weight: 300;
-  font-size: 12vw;
-`;
+// const TitleHome = styled(Title)`
+//   margin-bottom: 10px;
+//   font-weight: 300;
+//   font-size: 12vw;
+// `;
 
-const Subtitle = styled.p`
-  font-size: 6vw;
-  font-style: italic;
-  color: white;
-`;
+// const Subtitle = styled.p`
+//   font-size: 6vw;
+//   font-style: italic;
+//   color: white;
+// `;
 
-const ReactImg = styled.img`
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translate(-50%);
-  object-fit: contain;
-  height: 150%;
-`;
+// const ReactImg = styled.img`
+//   position: absolute;
+//   bottom: 0;
+//   left: 50%;
+//   transform: translate(-50%);
+//   object-fit: contain;
+//   height: 150%;
+// `;
 
-const PhotoWrapper = styled.div`
-  position: absolute;
-  bottom: 40vh;
-  margin-left: 50%;
-  transform: translate(-50%);
-  width: 150px;
-  height: 150px;
-`;
+// const PhotoWrapper = styled.div`
+//   position: absolute;
+//   bottom: 40vh;
+//   margin-left: 50%;
+//   transform: translate(-50%);
+//   width: 150px;
+//   height: 150px;
+// `;
 
-const Square = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  background-color: grey;
-`;
+// const Square = styled.div`
+//   position: absolute;
+//   bottom: 0;
+//   width: 100%;
+//   height: 100%;
+//   background-color: grey;
+// `;
 
-const Photo = styled.img`
-  position: absolute;
-  bottom: 0;
-  object-fit: contain;
-  width: 100%;
-`;
+// const Photo = styled.img`
+//   position: absolute;
+//   bottom: 0;
+//   object-fit: contain;
+//   width: 100%;
+// `;
 
-const ButtonsWrapper = styled.div`
-  position: absolute;
-  bottom: 20vh;
-  margin-left: 50%;
-  transform: translate(-50%);
-  width: 70vw;
-  display: flex;
-  justify-content: space-between;
-`;
+// const ButtonsWrapper = styled.div`
+//   position: absolute;
+//   bottom: 20vh;
+//   margin-left: 50%;
+//   transform: translate(-50%);
+//   width: 70vw;
+//   display: flex;
+//   justify-content: space-between;
+// `;
 
-const HomeButton = styled(Button)`
-  width: 90px;
-  height: 30px;
-`;
+// const HomeButton = styled(Button)`
+//   width: 90px;
+//   height: 30px;
+// `;
 
 const Home = () => {
   return (
---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { Title } from "../styles/Common";
+
+const Wrapper = styled.div`
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(67, 69, 71);
+  z-index: 10;
+`;
+
+const LogoWrapper = styled.div`
+  position: absolute;
+  top: 13vh;
+  margin-left: 50%;
+  transform: translate(-50%);
+  text-align: center;
+  height: 140px;
+  width: 100%;
+`;
+
+const TitleHome = styled(Title)`
+  margin-bottom: 10px;
+  font-weight: 300;
+  font-size: 12vw;
+`;
+
+const Subtitle = styled.p`
+  font-size: 6vw;
+  font-style: italic;
+  color: white;
+`;
+
+const ReactImg = styled.img`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%);
+  object-fit: contain;
+  height: 150%;
+`;
+
+const PhotoWrapper = styled.div`
+  position: absolute;
+  bottom: 40vh;
+  margin-left: 50%;
+  transform: translate(-50%);
+  width: 150px;
+  height: 150px;
+`;
+
+const Square = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: grey;
+`;
+
+const Photo = styled.img`
+  position: absolute;
+  bottom: 0;
+  object-fit: contain;
+  width: 100%;
+`;
+
+const ButtonsWrapper = styled.div`
+  position: absolute;
+  bottom: 20vh;
+  margin-left: 50%;
+  transform: translate(-50%);
+  width: 70vw;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HomeButton = styled(Button)`
+  width: 90px;
+  height: 30px;
+`;
+
+export {
+  Wrapper,
+  LogoWrapper,
+  TitleHome,
+  Subtitle,
+  ReactImg,
+  PhotoWrapper,
+  Square,
+  Photo,
+  ButtonsWrapper,
+  HomeButton
+};
