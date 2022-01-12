3d91aa1ad6cfdca3885bbb4ac5a8713707e06814
viktor <vikhulei@gmail.com>
2022-01-12 13:41:37 +0200

    images added to projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,18 +2,22 @@ import { Title } from "../styles/Common";
 import { Wrapper, ImagesWrapper, Image } from "../styles/ProjectsStyles";
 import construct from "../assets/construct.png";
 import safety from "../assets/safety.png";
+import church from "../assets/church.png";
+import hrm from "../assets/hrm.png"
+import quiz from "../assets/quiz.png"
+import words from "../assets/words.png"
 
 const Projects = () => {
   return (
     <Wrapper>
       <Title>Projects</Title>
       <ImagesWrapper>
+        <Image src={safety} alt="safety" />
+        <Image src={church} alt="church" />
+        <Image src={words} alt="words" />
         <Image src={construct} alt="construct" />
-        <Image src={safety} alt="safetz" />
-        <Image src={safety} alt="safetz" />
-        <Image src={construct} alt="construct" />
-        <Image src={construct} alt="construct" />
-        <Image src={safety} alt="safetz" />
+        <Image src={hrm} alt="hrm" />
+        <Image src={quiz} alt="quiz" />
       </ImagesWrapper>
     </Wrapper>
   );
