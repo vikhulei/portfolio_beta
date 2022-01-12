2ed9d0451590d203950537994a28a429be98e543
viktor <vikhulei@gmail.com>
2022-01-12 14:04:42 +0200

    TextWrapper made shoter in About page

--- src/styles/AboutStyles.js
+++ src/styles/AboutStyles.js
@@ -12,7 +12,7 @@ const TextWrapper = styled.div`
   position: relative;
   background-color: lightgrey;
   box-shadow: 0 0 40px lightgrey;
-  height: 70vh;
+  height: 60vh;
   width: 80vw;
   top: 5%;
   margin: 0 auto;
