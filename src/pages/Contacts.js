c7a0dab4a278e13350388d103fc5e7445df6c078
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-01-13 20:54:40 +0200

    icons added to Contact page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,16 @@
 import { Title } from "../styles/Common";
-import { Wrapper, ContentWrapper, Image, IconsWrapper, WhApp } from "../styles/ContactsStyles";
+import {
+  Wrapper,
+  ContentWrapper,
+  Image,
+  IconsWrapper
+} from "../styles/ContactsStyles";
 import contacts from "../assets/contacts.png";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import * as Fa from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
 
 const Contacts = () => {
   return (
@@ -9,7 +19,14 @@ const Contacts = () => {
       <ContentWrapper>
         <Image src={contacts} alt="contacts" />
         <IconsWrapper>
-          <WhApp style={{"fill": "green"}}/>
+          <Fa.FaViber fontSize="3rem" color="#AD35DE" />
+          <Fa.FaWhatsapp fontSize="3rem" color="#31BE11" />
+          <Fa.FaLinkedin fontSize="3rem" color="#31BE11" />
+          <Fa.FaGithub fontSize="3rem" color="#31BE11" />
+          <Fa.FaTelegram fontSize="3rem" color="#31BE11" />
+          <Fa.FaFacebookMessenger fontSize="3rem" color="#31BE11" />
+          <AiOutlineMail fontSize="3rem" color="#31BE11" />
+          <AiOutlinePhone fontSize="3rem" color="#31BE11" />
         </IconsWrapper>
       </ContentWrapper>
     </Wrapper>
--- src/styles/ContactsStyles.js
+++ src/styles/ContactsStyles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Wrapper = styled.div`
   position: relative;
@@ -9,41 +9,35 @@ const Wrapper = styled.div`
   background-color: rgb(57, 71, 81);
 `;
 
-const ContentWrapper = styled.div `
+const ContentWrapper = styled.div`
   position: relative;
   margin: 0 auto;
   top: 5vh;
   width: 70vw;
 `;
 
-const Image = styled.img `
+const Image = styled.img`
   position: absolute;
   width: 100%;
   object-fit: contain;
 `;
 
-const ContactsWrapper = styled.div `
+const ContactsWrapper = styled.div`
   position: relative;
   margin: 0 auto;
   top: 15vh;
   width: 70vw;
 `;
 
-const PhoneNumber = styled.p `
+const PhoneNumber = styled.p``;
 
-`;
+const EmailAddress = styled.p``;
 
-const EmailAddress = styled.p `
+const IconsWrapper = styled.div``;
 
-`;
-
-const IconsWrapper = styled.div `
-
-`;
-
-const WhApp = styled(WhatsAppIcon) `
+const WhApp = styled(WhatsAppIcon)`
   fill: green;
-`
-
+  width: 50px;
+`;
 
 export { Wrapper, ContentWrapper, Image, IconsWrapper, WhApp };
