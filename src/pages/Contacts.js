db36f75cd9ba0f808bb6f7d03880230b4f78cfaa
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-01-17 12:02:04 +0200

    All links to messengers added

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -11,7 +11,7 @@ import {
   EmailAddress,
   ContactsWrapper
 } from "../styles/ContactsStyles";
-// import contacts from "../assets/contacts.png";
+import contacts from "../assets/contacts.png";
 import * as Fa from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import { AiOutlinePhone } from "react-icons/ai";
@@ -22,7 +22,7 @@ const Contacts = () => {
       <Title>Contacts</Title>
 
       <ContentWrapper>
-        {/* <Image src={contacts} alt="contacts" /> */}
+        <Image src={contacts} alt="contacts" />
         <ContactsWrapper>
           <AiOutlinePhone fontSize="2.2rem" color="#31BE11" />
           <PhoneNumber>+380637931590</PhoneNumber>
@@ -37,19 +37,19 @@ const Contacts = () => {
             {" "}
             <Fa.FaFacebookMessenger color="#0037FD" />{" "}
           </Icon>
-          <Icon href="https://wa.me/+380639731590" target="_blank">
+          <Icon href="https://wa.me/+380637931590" target="_blank">
             <Fa.FaWhatsapp fontSize="2.5rem" color="#31BE11" />{" "}
           </Icon>
-          <Icon href="#" target="_blank">
+          <Icon href="https://telegram.me/vikhulei" target="_blank">
             <Fa.FaTelegram fontSize="2.5rem" color="#4BB6F2" />
           </Icon>
-          <Icon href="#" target="_blank">
+          <Icon href="viber://chat/?number=%2B380637931590" target="_blank">
             <Fa.FaViber fontSize="2.5rem" color="#B017C4" />
           </Icon>
-          <Icon href="#" target="_blank">
+          <Icon href="https://www.linkedin.com/in/vikhulei" target="_blank">
             <Fa.FaLinkedin fontSize="2.5rem" color="#3C65F3" />
           </Icon>
-          <Icon href="#" target="_blank">
+          <Icon href="https://github.com/vikhulei" target="_blank">
             <Fa.FaGithub fontSize="2.5rem" color="white" />
           </Icon>
         </IconsWrapper>
