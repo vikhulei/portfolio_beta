fb15619a8d3fbe1c4f0f1322ce4c30e12903863f
vikhulei <vikhulei@gmail.com>
2022-06-06 22:33:49 +0100

    navbar styling adjusted

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,62 +3,58 @@ import Hamburger from "hamburger-react";
 
 const NavWrapper = styled.div`
   position: fixed;
-  top: 0;
-  left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
-  transition: 0ms;
-  height: 100vh;
-  width: 100%;
-  background-color: grey;
-  z-index: 99;
-  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  align-items: center;
-  @media screen and (min-width: 800px) {
-    left: 5vw;
-    transition: 0ms;
-    width: fit-content;
-    height: 500px;
-    top: 45%;
-    transform: translateY(-50%);
-    background-color: rgba(30, 30, 30, 0);
+  align-items: left;
+  left: 5vw;
+  width: fit-content;
+  height: 100vh;
+  padding: 15% 0;
+  z-index: 98;
+  
+  @media screen and (max-width: 800px) {
+    transition: 500ms;
+    left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+    padding: 0;
+    width: 100%;
+    background-color: grey;
   }
-`;
+`
 
 const NavLink = styled.a`
-  position: relative;
-  text-decoration: none;
-  color: white;
+position: relative;
+text-decoration: none;
+color: white;
+border-bottom: none;
+font-size: 2rem;
+&:active {
+  transform: translate(3px, 3px);
+  color: lightgrey;
+}
+
+@media screen and (max-width: 800px) {
   font-size: 1.5rem;
+  border-bottom: solid 2px black;
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-bottom: solid 2px black;
-  &:active {
-    transform: translate(3px, 3px);
-    color: lightgrey;
-  }
-  @media screen and (min-width: 800px) {
-    border-bottom: none;
-    justify-content: left;
-    font-size: 2rem;
-  }
-`;
+}
+`
 
 const HamWrapper = styled.div`
+display: none;
+@media screen and (max-width: 800px) {
   position: fixed;
   width: 95%;
   display: flex;
   justify-content: right;
   align-items: center;
   z-index: 99;
-  @media screen and (min-width: 800px) {
-    display: none;
   }
-`;
+`
 
 const SideBar = ({ isOpen, setOpen, toggleHam }) => {
   return (
